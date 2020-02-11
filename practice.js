54647c1f528dec06e3745cd471ef463d9e849b03
Noman Rafiq <59981447+imnomee@users.noreply.github.com>
2020-02-11 10:47:19 +0000

    Update practice.js

--- practice.js
+++ practice.js
@@ -1,70 +1,70 @@
-// // // const rambo = {
-// // //     animal: "dog",
-// // //     age: 1,
-// // //     breed: "bull",
-// // //     bark: function() {
-// // //         console.log("woof");
-// // //     }
-// // // };
+// const rambo = {
+//     animal: "dog",
+//     age: 1,
+//     breed: "bull",
+//     bark: function() {
+//         console.log("woof");
+//     }
+// };
 
-// // // const fruits = ["apple", "pear", "strawberry"];
-// // // fruitsLength = fruits.length;
-// // // // console.log(fruits[0]);
-// // // // console.log(fruitsLength);
+// const fruits = ["apple", "pear", "strawberry"];
+// fruitsLength = fruits.length;
+// // console.log(fruits[0]);
+// // console.log(fruitsLength);
 
-// // // // console.log(rambo.animal, rambo.breed);
-// // // console.log(rambo["age"]);
-// // // console.log(rambo["bark"]());
+// // console.log(rambo.animal, rambo.breed);
+// console.log(rambo["age"]);
+// console.log(rambo["bark"]());
 
-// // // const teacher = {
-// // //     firstName: "Ashley",
-// // //     lastName: "Boucher",
-// // //     printName: function() {
-// // //         console.log(this.firstName + this.lastName);
-// // //     }
-// // // };
+// const teacher = {
+//     firstName: "Ashley",
+//     lastName: "Boucher",
+//     printName: function() {
+//         console.log(this.firstName + this.lastName);
+//     }
+// };
 
-// // // rambo.age = 3;
-// // // rambo["breed"] = "bull dog";
-// // // console.log(rambo.breed);
-// // // rambo.color = "black";
-// // // console.log(rambo);
-// // const myString = {
-// //     string: "Programming with Treehouse is fun!",
-// //     countWords: function() {
-// //         const wordArray = this.string.split(" ");
-// //         return wordArray.length;
-// //     }
-// // };
-// // myString.string = "I love full stack javaScript";
-// // myString.characters = myString.string.length;
+// rambo.age = 3;
+// rambo["breed"] = "bull dog";
+// console.log(rambo.breed);
+// rambo.color = "black";
+// console.log(rambo);
+// const myString = {
+//     string: "Programming with Treehouse is fun!",
+//     countWords: function() {
+//         const wordArray = this.string.split(" ");
+//         return wordArray.length;
+//     }
+// };
+// myString.string = "I love full stack javaScript";
+// myString.characters = myString.string.length;
 
-// // var numWords = myString.countWords();
+// var numWords = myString.countWords();
 
-// // //Create an empty method called stringGPA() and add it to the Student class
+//Create an empty method called stringGPA() and add it to the Student class
 
-// // class Student {
-// //     constructor(gpa, courses) {
-// //         this.gpa = gpa;
-// //         this.courses = courses;
-// //     }
+// class Student {
+//     constructor(gpa, courses) {
+//         this.gpa = gpa;
+//         this.courses = courses;
+//     }
 
-// //     printGPA() {
-// //         console.log(this.gpa);
-// //     }
+//     printGPA() {
+//         console.log(this.gpa);
+//     }
 
-// //     addCourse(course) {
-// //         this.courses.push(course);
-// //     }
-// // }
+//     addCourse(course) {
+//         this.courses.push(course);
+//     }
+// }
 
-// // const ashley = new Student(3.9);
-// // const birthday = new Date();
-// // const day1 = birthday.getDay();
-// // // Sunday - Saturday : 0 - 6
+// const ashley = new Student(3.9);
+// const birthday = new Date();
+// const day1 = birthday.getDay();
+// // Sunday - Saturday : 0 - 6
 
-// // console.log(day1);
-// // // expected output: 2
+// console.log(day1);
+// expected output: 2
 // class Student {
 //     constructor(gpa, credits) {
 //         this.gpa = gpa;
